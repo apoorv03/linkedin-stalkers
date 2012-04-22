e99b82bb6abf5d550231652cb93fedb93df61ed8
Apoorv Agrawal <apoorv03@gmail.com>
2012-04-21 18:39:14 -0700

    view filev1

---
+++ views/header.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<style type='text/css' media='all'>@import url('http://ec2-184-169-245-130.us-west-1.compute.amazonaws.com/ci/application/linked.css');</style> 
+	<link rel='stylesheet' type='text/css' media='all' href='http://ec2-184-169-245-130.us-west-1.compute.amazonaws.com/ci/application/linked.css' />
+</head>
+
+<body>
+	<div id = "content">
+	<h1>Scrape your LinkedIN Stalkers</h1>
+	<p>apoorv03(at)gmail(dot)com</p>
